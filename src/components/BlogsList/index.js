a55b6f2acb20979daf1ad12c46e78dddf7c41f79
parameshwar vhanhuve <vhanhuveparmeshwar@gmail.com>
2023-04-27 08:23:27 +0000

    Routing Part-2

---
+++ src/components/BlogsList/index.js
@@ -0,0 +1,48 @@
+import {Component} from 'react'
+
+import Loader from 'react-loader-spinner'
+
+import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css'
+
+import BlogItem from '../BlogItem'
+
+import './index.css'
+
+class BlogsList extends Component {
+  state = {blogsData: [], isLoading: true}
+
+  componentDidMount() {
+    this.getBlogData()
+  }
+
+  getBlogData = async () => {
+    const response = await fetch('https://apis.ccbp.in/blogs')
+    const data = await response.json()
+    const updatedData = data.map(eachData => ({
+      id: eachData.id,
+      author: eachData.author,
+      title: eachData.title,
+      imageUrl: eachData.image_url,
+      avatarUrl: eachData.avatar_url,
+      topic: eachData.topic,
+    }))
+
+    console.log(updatedData)
+    this.setState({blogsData: updatedData, isLoading: false})
+  }
+
+  render() {
+    const {blogsData, isLoading} = this.state
+    return (
+      <div className="blog-list-container">
+        {isLoading ? (
+          <Loader type="TailSpin" color="#00BFFF" height={50} width={50} />
+        ) : (
+          blogsData.map(item => <BlogItem blogData={item} key={item.id} />)
+        )}
+      </div>
+    )
+  }
+}
+
+export default BlogsList
